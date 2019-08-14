c938f35dc104c1634e1b5a0e7376c099d53e6f92
mouaedkabouk <mkabouk@gmail.com>
2019-08-14 18:08:51 +0200

    first commit

---
+++ exercises.js
@@ -0,0 +1,82 @@
+// percent
+let first = 100 * 17 ;
+let secund = 30 ;
+let result = first / secund ;
+console.log(result+ ` % ` );
+
+// working with average
+
+let total = 8+ 6+ 5+ 9+ 8+ 2+ 1+ 8.5+ 7+ 4;
+let NOe = 10 ;
+let average = total / NOe ;
+console.log(average);
+
+
+let scores = 75 + 70 + 85 + 90 + 100  ;
+let tests = 6
+let lastAverage = 85 ;
+let messingTestScore ;
+messingTestScore = (lastAverage * tests ) - scores ; 
+console.log(messingTestScore);
+
+
+
+let rest =100 *2 ;
+let precentAverage = 80 ;
+let minimum =rest / precentAverage ;
+console.log(minimum + `%`);
+
+
+let firstBill = 22.35 ;
+let secundBill = 26.67 ;
+let therdBill = 35.92 ;
+let firstTip =firstBill + (firstBill / 100) *10;
+let secundTip =secundBill + (secundBill / 100) *15;
+let therdTip = therdBill + (therdBill / 10) *2
+let wholePrice = `${firstTip}€ 
+ ${secundTip}€
+  ${therdTip}€` ;
+console.log(wholePrice);
+
+
+
+let squareSide = 4.75 ;
+let perimeter =squareSide * 4 ;
+console.log(perimeter + `cm`);
+
+let triangleFirstSide = 5 ;
+let triangleSecundSide = 6 ;
+let TriangleTherdSide = 7 ;
+let trianglePerimeter = triangleFirstSide + triangleSecundSide+ TriangleTherdSide ;
+console.log(trianglePerimeter + `cm`);
+
+let square  = 5;
+let squareArea = square * square ;
+console.log(squareArea + `cm`);
+
+let triangleSideOne = 5 ;
+let triangleSideTow = 6 ; 
+let triangleSideThree = 7 ;
+let pTriangle = 18 / 2 ;
+let triangleResult = ( pTriangle*(pTriangle-triangleSideOne)*(pTriangle-triangleSideTow)*(pTriangle-triangleSideThree));
+let triangleArea = triangleResult / 14.6969384567;
+console.log(triangleArea + ` cm`);
+
+let cubeSide = 9 ;
+let cubeVolume = (cubeSide *cubeSide* cubeSide) ;
+console.log(cubeVolume + ` cm³`);
+
+
+let ticketsPrices = 6450 ;
+let oneTicketsPrice = 15 ;
+let peoplePur = ticketsPrices / oneTicketsPrice ;
+console.log(peoplePur + ` persons`);
+
+let sylviasIncome = 500 ;
+let year = 12 ;
+let month = 4 ;
+let incomeYear = (year * month) * sylviasIncome ;
+console.log(incomeYear + ` $`);
+
+
+
---
+++ index.js
@@ -0,0 +1,77 @@
+var FirstName,LastName;
+FirstName="Mouaed";
+LastName=" Kabouk ";
+console.log(FirstName,LastName);
+
+var Old;
+Old="22";
+console.log(Old);
+
+var Country;
+Country = "Syria";
+console.log(Country);
+
+var Work;
+Work ="Programmer"
+console.log(Work);
+
+
+var firstName = "John";
+var lastName = "Smith";
+var old = 43;
+var job = "teacher";
+var city = "Berlin"
+var result = firstName + " " + lastName + " is a " + old + " Years old " + job + " who lives in " + city;
+
+console.log(result);
+
+
+let length = 16.78; 
+let Name = "Johnson"; 
+let car = ""; 
+let isOpen = true;
+let nothing;
+
+console.log(typeof length);
+console.log(typeof Name);
+console.log(typeof car);
+console.log(typeof isOpen);
+console.log(typeof nothing);
+
+
+var ageMark = 20;
+var agrJohn = "40";
+console.log(typeof ageMark);
+console.log(typeof agrJohn);
+
+var john =40;
+var mark = 20 ;
+console.log((john > mark) ? "john is older then mark: true" : "john is not older then mark: false");
+
+
+var a , b , c ;
+a = 10 ;
+b = 30 ;
+c = 30 ;
+console.log(a === c);
+console.log(a === b);
+console.log(c === b);
+console.log(typeof (a===b));
+console.log(typeof b);
+console.log(typeof c);
+
+
+var programmingLanguage = "javaScript" ;
+var isFun = true ; 
+var result = programmingLanguage + " is Fun: " + isFun ;
+console.log(result);
+
+var test ;
+console.log(typeof test);
+var undefined = "noValue" ;
+var result = "because there is " + undefined ;
+console.log(result);
+
+
+var calculation= (2*5)+4/(2-5) ;
+console.log(calculation);
