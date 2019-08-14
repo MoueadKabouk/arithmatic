dc642e6b82b2151e7541196d9ff2f99be5bc340b
mouaedkabouk <mkabouk@gmail.com>
2019-08-15 01:03:40 +0200

    exercises from 14.08.2019

---
+++ ex3.js
@@ -0,0 +1,54 @@
+let string = `deutschland` ;
+console.log(string.length);
+
+console.log(string [2],string [6]);
+
+let = `javaScript` ;
+console.log(let.length);
+console.log(let[0]+let [4]);
+
+
+let walking = `i can walk in the park all the day`;
+console.log(walking[18]+walking[19]+walking[20]+walking[21]+walking[22]);
+
+
+let using = `JavaScript` ;
+console.log(`a`+using[4]+`c`);
+
+
+let test = 'HELLO WORLD';
+let result = test.toLowerCase()
+console.log(result);
+
+
+let test2 = `hello earthling`;
+console.log(test2.toUpperCase());
+
+
+let str = `nice shoes`
+let firstLetter = str.includes("n")
+let secundLetter = str.includes("i");
+console.log(firstLetter);
+console.log(secundLetter);
+
+
+let js = `JavaScript`;
+console.log(js[0] + js + js[0]);
+
+let chars = `JavaScript`;
+console.log(chars[7]+chars[8]+chars[9]+chars+chars[7]+chars[8]+chars[9]);
+
+
+let upper = `barcelona is always a good idea `;
+console.log(upper.toUpperCase());
+
+
+let firstName = `moudy`;
+let city = `  Berlin`;
+let job =`programmer`;
+console.log(`my name is: `+firstName+`, i live in `+city+` and iam a `+job);
+ 
+
+
+let fox = `the quick brown fox`;
+console.log(fox[0].toUpperCase());
