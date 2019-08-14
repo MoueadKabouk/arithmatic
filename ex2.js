fd806a5973f720c5799d9149c9a38bb5b020582b
mouaedkabouk <mkabouk@gmail.com>
2019-08-14 18:49:17 +0200

    canDrive??!

---
+++ ex2.js
@@ -0,0 +1,14 @@
+let person = 20;
+
+
+if (person > 18){
+    console.log('yes, you can drive !  Hope you have a good license');
+    
+}else if (person == 18) {
+    console.log('you need to practice');
+}else {
+    console.log('nope, you are too young');
+    
+}
+
+
