94a537765d8871e5b57c6dc40351a392a44a1d47
mouaedkabouk <mkabouk@gmail.com>
2019-08-20 11:12:52 +0200

    math object finished

---
+++ mathOB.js
@@ -0,0 +1,25 @@
+const findLowestNumber = Math.min(99 , 55 , 83 , 14 ,72);
+console.log(findLowestNumber);
+
+const findHighestNumber = Math.max (100 , 285 , 40*50 , 73);
+console.log(findHighestNumber);
+
+
+const roundingUpA = Math.ceil(3321.32321);
+const roundingUpB = Math.ceil(326.76);
+const roundingUpC =Math.ceil(76788.7)
+console.log(roundingUpA);
+console.log(roundingUpB);
+console.log(roundingUpC);
+
+const roundingDownA = Math.floor(3321.32321);
+const roundingDownB = Math.floor(326.76);
+const roundingDownC = Math.floor(76788.7);
+console.log(roundingDownA);
+console.log(roundingDownB);
+console.log(roundingDownC);
+
+const min = 1 ;
+const max = 6;
+const random = Math.floor(Math.random()* (max - min +1))+ min;
+console.log(random);
